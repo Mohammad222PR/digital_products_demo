c1ff698cc90e4793161dffcb0871d6da3c4849db
Mohammad222PR <mohammades13851@gmail.com>
2023-03-16 15:21:15 +0330

    Product models added

--- products/models.py
+++ products/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='title', max_length=50)
+    description = models.TextField('description', blank=True)
+    avatar = models.ImageField(blank=True, upload_to='categories')
+    is_enable = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+class Product(models.Model):
+    title = models.CharField(verbose_name='title', max_length=50)
+    description = models.TextField('description', blank=True)
+    avatar = models.ImageField(blank=True, upload_to='products/')
+    is_enable = models.BooleanField(default=True)
+    categories = models.ManyToManyField('Category', blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+
+
+class File(models.Model):
+    product = models.ForeignKey('Product', blank=True, on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='title', max_length=50)
+    description = models.TextField('description', blank=True)
+    file = models.FileField('file', upload_to='file/%y/%m)/%d')
+    avatar = models.ImageField(blank=True, upload_to='categories')
+    is_enable = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'files'
+        verbose_name = 'file'
+        verbose_name_plural = 'files'
